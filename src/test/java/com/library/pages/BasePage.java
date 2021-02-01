44f25959ea324497425e5abaff907ac997b9653e
JHY34 <hamityilmaz34@gmail.com>
2021-01-31 20:37:37 -0600

    Util Classes are created

--- src/test/java/com/library/pages/BasePage.java
+++ src/test/java/com/library/pages/BasePage.java
@@ -1,4 +1,13 @@
 package com.library.pages;
 
+import org.openqa.selenium.support.PageFactory;
+
+import java.sql.Driver;
+
 public class BasePage {
+
+    public BasePage () {
+        PageFactory.initElements(Driver.getDriver() , this);
+    }
+
 }
